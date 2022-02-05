2f8db93cbb72231e6c9fc8450cfab549df81a49d
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-05 18:46:48 +0100

    user router created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const dotenv = require('dotenv')
+const UserRoute = require('./routes/users')
 
 dotenv.config()
 
@@ -13,6 +14,19 @@ mongoose
   .catch((err) => {
     console.log(err)
   })
+
+//Middleware
+app.use(express.json())
+app.use(helmet())
+app.use(morgan('common'))
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ msg: 'Welcome to homepage' })
+})
+
+app.get('/users', (req, res) => {
+  return res.status(200).json({ msg: 'Welcome to user page' })
+})
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Backend server is running!')
