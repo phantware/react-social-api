0024c0c39b5ff9e49ad51b9510e1647691fad5d2
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-06 16:44:05 +0100

    User model updated

--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,9 @@ router.get('/', (req, res) => {
   res.status(200).json({ msg: 'you are welcome to user page' })
 })
 
+//Update User
+//Delete User
+//Get a User
+//Follow a User
+//Unfollow a User
 module.exports = router
