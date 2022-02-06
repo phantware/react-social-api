d2674a3556b8538250aa8564c51ba6bb4beef87c
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-06 14:20:55 +0100

    Register route created

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,29 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 router.get('/', (req, res) => {
   res.status(200).json({ msg: 'you are welcome to auth page' })
 })
 
 router.post('/register', async (req, res) => {
-  let { username, email, password } = req.body
-  const newUser = new User({
-    username,
-    email,
-    password,
-  })
   try {
+    //Generate new password
+
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(req.body.password, salt)
+
+    // create new user
+
+    let { username, email, password } = req.body
+    const newUser = new User({
+      username,
+      email,
+      password: hashPassword,
+    })
+
+    // save users and return response
+
     const user = await newUser.save()
     return res.status(200).json(user)
   } catch (error) {
