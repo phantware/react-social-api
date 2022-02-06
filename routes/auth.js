9f7f1dbd08967dec468c579311c34ee114db1bdd
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-06 01:13:29 +0100

    user model updated

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
 const router = require('express').Router()
+const User = require('../models/User')
 
 router.get('/', (req, res) => {
   res.status(200).json({ msg: 'you are welcome to auth page' })
+})
+
+router.post('/register', async (req, res) => {
+  let { username, email, password } = req.body
+  const newUser = new User({
+    username,
+    email,
+    password,
+  })
+  try {
+    const user = await newUser.save()
+    return res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
