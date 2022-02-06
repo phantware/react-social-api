9cd912bb68ee2a3d72eee07b9ab7bc3ab92d123f
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-06 19:06:35 +0100

    Followers route implemented

--- routes/users.js
+++ routes/users.js
@@ -58,5 +58,24 @@ router.get('/:id', async (req, res) => {
 })
 
 //Follow a User
+router.put('/:id/follow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id)
+      const currentUser = await User.findById(req.body.userId)
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } })
+        await currentUser.updateOne({ $push: { followings: req.params.id } })
+        return res.status(200).json({ msg: 'User has been followed' })
+      } else {
+        return res.status(403).json({ msg: 'You already follow this user' })
+      }
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(403).json({ msg: 'You cant follow yourself' })
+  }
+})
 //Unfollow a User
 module.exports = router
