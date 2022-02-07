2488f1c405ff00ef722e9468a160f4151d77076d
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-07 10:03:55 +0100

    timeline posts created

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Post = require('../models/Post')
+const User = require('../models/User')
 
 //create a post
 router.post('/', async (req, res) => {
@@ -72,5 +73,19 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-// get timeline posts
+router.get('/timeline/:userId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.params.userId)
+    const userPosts = await Post.find({ userId: currentUser._id })
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId })
+      })
+    )
+    res.status(200).json(userPosts.concat(...friendPosts))
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 module.exports = router
