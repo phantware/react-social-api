3d83bbcfeaf3f62d979020eb7087c3ac21c3f282
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-07 07:03:40 +0100

    Update post route created

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,6 @@ const router = require('express').Router()
 const Post = require('../models/Post')
 
 //create a post
-
-//update a post
 router.post('/', async (req, res) => {
   const newPost = new Post(req.body)
   try {
@@ -11,6 +9,23 @@ router.post('/', async (req, res) => {
     return res.status(200).json(savePost)
   } catch (error) {
     return res.status(500).json({ msg: error })
+  }
+})
+
+//update a post
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body })
+      return res
+        .status(200)
+        .json({ msg: 'The post has been updated successfully' })
+    } else {
+      res.status(400).json({ msg: 'You can update only your post' })
+    }
+  } catch (error) {
+    return res.status(500).json(error)
   }
 })
 //delete a post
