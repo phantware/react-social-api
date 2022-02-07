ea32ef936c31b2496179d51c788caf940146de24
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-02-07 07:24:58 +0100

    Like/dislike post route created

--- routes/posts.js
+++ routes/posts.js
@@ -46,7 +46,22 @@ router.delete('/:id', async (req, res) => {
   }
 })
 
-//like a post
+//like / dislike a post
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } })
+      return res.status(200).json({ msg: 'The post has been liked' })
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } })
+      return res.status(200).json({ msg: 'The post has been disliked' })
+    }
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
+
 //get a post
 // get timeline posts
 module.exports = router
